ed2352a79830205f952567543f5655d53fa50e50
Julian Ladd <julianrladd@gmail.com>
2023-07-19 12:25:03 -0500

    updates for components and carousel

---
+++ portfolio/src/Components/Projects.js
@@ -0,0 +1,45 @@
+export const Projects = () =>{
+    const projects =[
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg4,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg5,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg6,
+        },
+    ];
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col></Col>
+                </Row>
+             </Container>
+        </section>
+    )
+}
+
+export default Projects;
