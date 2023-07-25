f18db1b39063dbf2441972e21c63ff9e3f112288
Julian Ladd <julianrladd@gmail.com>
2023-07-25 14:18:14 -0500

    starting to add card displays for each project

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import NavBar from './Components/NavBar';
-import Banner from './Components/Banner';
-import Skills from './Components/Skills';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Banner from "./Components/Banner";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-    <NavBar/>
-    <Banner/>
-    <Skills/>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -1,45 +1,72 @@
-export const Projects = () =>{
-    const projects =[
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg4,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg5,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg6,
-        },
-    ];
-    return (
-        <section className="project" id="project">
-            <Container>
-                <Row>
-                    <Col></Col>
-                </Row>
-             </Container>
-        </section>
-    )
-}
+import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
+import ProjectCard from "./ProjectCard";
+import colorSharp2 from "../Assets/img/color-sharp2.png";
+import projImg1 from "../Assets/img/project-img1.png";
+import projImg2 from "../Assets/img/project-img2.png";
+import projImg3 from "../Assets/img/project-img3.png";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              lacinia scelerisque blandit. Duis a lorem nulla.
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-center align-items-center"
+                id="pill-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-img-right" src={colorSharp2} />
+    </section>
+  );
+};
 
 export default Projects;
