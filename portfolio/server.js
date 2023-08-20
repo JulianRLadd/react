eb66f1d0e0713e83873782ba0cf3dfa7edae1741
Julian Ladd <julianrladd@gmail.com>
2023-08-20 16:17:26 -0500

    added code for footer and server, added boilerplate for newsletter and form submission

--- portfolio/server.js
+++ portfolio/server.js
@@ -13,9 +13,43 @@ app.listen(5000, () => console.log("Server is presently running"));
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "",
+    user: "**********@gmail.com",
     pass: "",
   },
 });
 
-contact;
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mal = {
+    from: name,
+    to: "kresoazazel@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `
+    <p>Name: ${name}</p>
+    <p>Email: ${email}</p>
+    <p>Phone: ${phone}</p>
+    <p>Message: ${message}</p>
+    `,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({
+        code: 200,
+        status: "Message Sent",
+      });
+    }
+  });
+});
---
+++ portfolio/src/Components/Newsletter.js
@@ -0,0 +1,11 @@
+export const Newsletter = ({ subscirbe, status, message }) => {
+  return (
+    <Col lg={12}>
+      <div className="newsletter-bx">
+        <Row>
+          <Col lg={12} md={6} />
+        </Row>
+      </div>
+    </Col>
+  );
+};
